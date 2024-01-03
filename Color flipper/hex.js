3d89f8d161fbb56a85266ae90ffe883309c63e07
shhiivm <shhiivm@pm.me>
2024-01-03 19:51:38 +0530

    hex code will generate any random color

---
+++ Color flipper/hex.js
@@ -0,0 +1,19 @@
+const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.querySelector("#btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+
+  for (i = 0; i < 6; i++) {
+    hexColor = hexColor + hex[RandomNumber()];
+  }
+
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+function RandomNumber() {
+  return Math.floor(Math.random() * 16);
+}
