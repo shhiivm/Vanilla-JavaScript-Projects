01782e477c9c075362742c94a07e82755f182f18
shhiivm <shhiivm@pm.me>
2024-01-05 23:41:24 +0530

    minor changes

--- Color flipper/index.js
+++ Color flipper/index.js
@@ -5,11 +5,7 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
   // get random number b/w 0-3
-  const randomNum = RandomNumber();
+  const randomNumber = Math.floor(Math.random() * colors.length);
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
-
-function RandomNumber() {
-  return Math.floor(Math.random() * hex.length);
-}
